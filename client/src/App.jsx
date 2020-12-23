663f25a5bc864d74485a0c0c2671ef3274174d15
Leo16012000 <67458956+Leo16012000@users.noreply.github.com>
2020-12-23 13:12:33 +0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "./App.scss";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
@@ -11,17 +11,17 @@ import NotFound from "./components/NotFound";
 import MyInfoTeacher from "./features/MyInfoTeacher";
 import MyPageTeacher from "./features/MyPageTeacher";
 
-import {Switch as Sw} from '@material-ui/core';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { Switch as Sw } from "@material-ui/core";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 function App() {
   const [state, setState] = React.useState({
     student: false,
-    teacher: false
+    teacher: false,
   });
-  
+
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
@@ -32,11 +32,24 @@ function App() {
         <FormControl component="fieldset">
           <FormGroup>
             <FormControlLabel
-              control={<Sw checked={state.student} onChange={handleChange} name="student" />}
+              control={
+                <Sw
+                  checked={state.student}
+                  onChange={handleChange}
+                  name="student"
+                />
+              }
               label="student"
             />
             <FormControlLabel
-              control={<Sw checked={state.teacher} color='primary' onChange={handleChange} name="teacher" />}
+              control={
+                <Sw
+                  checked={state.teacher}
+                  color="primary"
+                  onChange={handleChange}
+                  name="teacher"
+                />
+              }
               label="teacher"
             />
           </FormGroup>
@@ -44,21 +57,22 @@ function App() {
       </>
       {
         // (state.student || state.teacher) &&
-      <>
-        <Header />
+        <>
+          <Header />
 
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/mypage" component={MyPage} />
-          <Route path="/mypageTeacher" component={MyPageTeacher} />
-          <Route path="/register" component={Register} />
-          <Route path="/myinfo" component={MyInfo} />
-          <Route path="/MyInfoTeacher" component={MyInfoTeacher} />
-          <Route component={NotFound} />
-        </Switch>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/mypage" component={MyPage} />
+            <Route path="/mypageTeacher" component={MyPageTeacher} />
+            <Route path="/register" component={Register} />
+            <Route path="/myinfo" component={MyInfo} />
+            <Route path="/MyInfoTeacher" component={MyInfoTeacher} />
+            <Route component={NotFound} />
+          </Switch>
 
-        <div style={{ textAlign: "center" }}>Created by leo with ❤️</div>
-      </>}
+          <div style={{ textAlign: "center" }}>Created by leo with ❤️</div>
+        </>
+      }
     </>
   );
 }
